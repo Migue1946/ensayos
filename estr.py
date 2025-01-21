ba41ad76f1f20905808be888a3677c5b62324680
Migue <miguefavre@gmail.com>
2025-01-21 12:16:15 -0300

    resolviendo el px

--- estr.py
+++ estr.py
@@ -11,27 +11,17 @@ from scipy import stats
 
 
 df=pd.read_excel('C:/Users/Lenovo/Desktop/ENSAYOS/Ensayo.xlsx')
-#print(df)
-#st.write(df)
 
-
-mediat=df['tratado'].mean() #esta es la media. En describe figura como mean
-mediac=df['control'].mean()
-
-mediant=df['tratado'].median() # esta es la mediana. En describe en el valor que figura al 50%
-medianc=df['control'].median()
-
-df['tmedia']=mediat
-df['cmedia']=mediac
-
-ax=df.plot.line(x='sitio', y=['control','tratado','tmedia','cmedia'],linestyle='dashdot')
-plt.plot(df['tratado'].mean(), color='blue',linestyle='dashdot',label='Mediana de Tratado')
-#plt.show()
-opciones = ['control','tratado','cmedia','tmedia',['control','tratado'],['cmedia','tmedia'] ]
+opciones = ['control','tratado']
 
 with st.sidebar:
     parVariable=st.selectbox('Variable',options=opciones)
 
 st.title('Datos del Ensayo')
+st.write(df)
+
+
 fig= px.line(df,x='sitio',y= parVariable,title=f'{parVariable}')
 st.plotly_chart(fig, use_container_width=True)
+
+
